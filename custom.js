d7f06d99bcb5cc21d290749ade406fb7a5f988b4
Shifat <shifatmuhammad42@gmail.com>
2024-10-05 19:30:04 +0600

    Faching Problem

--- custom.js
+++ custom.js
@@ -1,9 +1,16 @@
 let string = "";
-let btns = document.querySelectorAll('.btn');
-Array.from(btns).forEach((btn)=>{
-    btn.addEventListener('Click', (e)=>{
-        console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
     })
 })
